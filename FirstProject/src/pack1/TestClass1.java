15173826a9ac7b5792ceaa40168f82f6ca9811cd
Aditya <ag89111@gmail.com>
2024-10-25 21:05:51 +0530

    First Commit

---
+++ FirstProject/src/pack1/TestClass1.java
@@ -0,0 +1,109 @@
+package pack1;
+
+public class TestClass1
+{
+	
+	static byte num9;
+	static boolean flag;
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println(num9);  // 0
+		System.out.println(flag);  // 
+		
+		byte num = 10;  		// -128 to +127   // 1 byte = 8 bit
+		short num2 = 20;     // 2 byte
+		int num3 = 30;      // 4 byte
+		long num4;   // variable initialization   // 8 byte 
+		num4 = 100;  // variable declaration
+		
+		
+		int num6 = 50;  // variable initialization  +  variable declaration
+		
+		float num7 = 50.6F;
+		double num8 = 60.55;
+		
+		char ch = 'c';  
+		boolean b = true;
+		
+		System.out.println(num2 + " " + num);
+		
+		
+		String s1 = "Test_1";  // String Literal  // 1 object is created
+		System.out.println("String value is : " + s1);
+		
+		
+		s1 = "tttt";
+		
+		
+		String s2 = new String("Test_2");  // 2 objects are created in memory
+		System.out.println(s2);
+		
+		
+		
+		String s3  = "Test_2";
+		
+		
+		
+		String s4 = new String("Test_2");
+		
+		
+		
+		// datatype [] variableName =  new dataType[count of data to be stored];
+		
+		
+
+		
+		
+		String [] sArray = new String[7];  // size/length of array = total number of elements that can be present in array
+		char [] chArray = new char[4];
+		
+		int [] array1 = new int[5];  // 20 bytes
+		
+		System.out.println(array1[0]);
+		System.out.println(array1[1]);
+		System.out.println(array1[2]);
+		System.out.println(array1[3]);
+		System.out.println(array1[4]);
+		
+		
+		array1[0] = 100;
+		array1[1] = 200;
+		array1[2] = 800;
+		array1[3] = 300;
+		array1[4] = 600;
+		
+		
+		System.out.println(array1[0]);
+		System.out.println(array1[1]);
+		System.out.println(array1[2]);
+		System.out.println(array1[3]);
+		System.out.println(array1[4]);
+		
+		// maximum index of array =  Array Length - 1
+		
+		System.out.println(array1.length);
+		
+		
+		
+		// dataType [] arrayName = {data,data,data};
+		
+		
+		String [] sArray2 = {"S1", "S2","S3","S4"};
+		
+		sArray2[2] = "S7";
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ FirstProject/src/pack1/TestClass2.java
@@ -0,0 +1,138 @@
+package pack1;
+
+import java.util.Iterator;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+
+		String s1 = "Hellouuhjvb";
+
+		System.out.println("s1 refers to : " + s1);
+
+		String s2 = s1.concat(" World");
+
+		System.out.println("s2 refers to : " + s2);
+
+//		while(boolean cond)
+//		{
+//			// code to repeat
+//		}
+
+//		5 times   // keyword in loops : break continue
+
+//		int i = 1;
+//
+//		while (i <= 5) 
+//		{
+//			
+//			if(i == 3)
+//			{
+//				continue;
+//			}
+//			else
+//			{
+//				System.out.println("ABC "+i);
+//				i++; 
+//			}
+//			
+//		}
+//		System.out.println("Out of while loop");
+		
+		// do while 
+		
+//		do
+//		{
+//			
+//		}
+//		while(boolean cond)
+		
+//		int num = 0;
+//		
+//		do 
+//		{
+//			System.out.println("ABC");
+//			num++;
+//		}
+//		while(num < 5);
+		
+		
+//		 For Loop
+		
+//				1				2,5,8		4,7
+//		for(intitialization; condition; increment/decreemnt)
+//		{
+//			 // code to repeat    // 3,6,9
+//		}
+		
+//		for(int i = 0; i<5; i++)
+//		{
+//			
+//			
+//			if( i == 3)
+//			{
+//				continue;
+//			}
+//				
+//			System.out.println("ABC "+i);
+//			
+//		}
+//		
+//		System.out.println("out of for loop");
+		
+		
+		int [] array1 = new int[10];  // 20 bytes
+		
+//		array1[0] = 100;
+//		array1[1] = 200;
+//		array1[2] = 800;
+//		array1[3] = 300;
+//		array1[4] = 600;
+		
+		
+//		System.out.println(array1[0]);
+//		System.out.println(array1[1]);
+//		System.out.println(array1[2]);
+//		System.out.println(array1[3]);
+//		System.out.println(array1[4]);
+		
+		int num =1; 
+//										2
+		for(int index = 0; index<=9; index++)
+		{
+			System.out.println(array1[index] = num);
+			num++;  // 2, 3
+		}
+
+		
+		System.out.println("FOR EACH LOOP");
+		
+		
+//		for( dataType ele : collectionToIterate)
+//		{
+//			// code to repeat 
+//		}
+		
+		
+		
+		// no need of indexing
+		
+		for( int num2 : array1)
+		{
+			System.out.println(num2);
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}

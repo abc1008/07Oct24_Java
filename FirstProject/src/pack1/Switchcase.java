e83254bee9718db785e18a8b7c6ba36f938882b1
Aditya <ag89111@gmail.com>
2024-10-28 20:43:47 +0530

    2nd commit

---
+++ FirstProject/src/pack1/Switchcase.java
@@ -0,0 +1,44 @@
+package pack1;
+
+public class Switchcase
+{
+	
+	
+	public static void main(String[] args)
+	{
+		
+		int dayNum = 2;
+		
+		switch(dayNum)
+		{
+		
+			case 0:
+				System.out.println("Sunday");
+				System.out.println("Sunday");
+				System.out.println("Sunday");
+				System.out.println("Sunday");
+				break;
+				
+			case 1:
+				System.out.println("Monday");
+				System.out.println("Monday");
+				System.out.println("Monday");
+				
+				break;
+				
+			case 2:
+				System.out.println("Tuesday");
+				System.out.println("Tuesday");
+				break;
+				
+			default:
+				System.out.println("dayNum value is incorrect");
+				break;
+		
+		}
+		
+		System.out.println("out of switch case");
+		
+	}
+
+}
